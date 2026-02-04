f7218c7e553aadd89812d16661c0174bb80f00db
Rohit <pakhrerohit281@gmail.com>
2026-02-04 17:27:59 +0530

    Feat: Provider and the themeProvide of the MUI added.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/global.css'
 import App from './App.jsx'
+import { Provider } from "react-redux"
+import { BrowserRouter } from 'react-router-dom'
+import { store } from './app/store.js'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme.js'
+import { CssBaseline } from '@mui/material'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
 )
