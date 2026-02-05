7b036ad9dee70687ca8414c9f3a70c7f107389a4
Rohit <pakhrerohit281@gmail.com>
2026-02-05 05:30:24 +0530

    Fix: MUI removed.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import './index.css'
-import './styles/global.css'
 import App from './App.jsx'
-import { Provider } from "react-redux"
-import { BrowserRouter } from 'react-router-dom'
 import { store } from './app/store.js'
-import { ThemeProvider } from '@mui/material/styles'
-import theme from './theme.js'
-import { CssBaseline } from '@mui/material'
+import { setupInterceptors } from './api/axios.js'
+
+setupInterceptors(store);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <App />
-        </ThemeProvider>
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
