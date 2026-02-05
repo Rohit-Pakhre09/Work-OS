d691de9df76025a73b15d2729a63432f2852491a
Rohit <pakhrerohit281@gmail.com>
2026-02-05 05:33:34 +0530

    Delete: Stats.js

--- frontend/src/api/stats.js
+++
@@ -1,7 +0,0 @@
-import { api } from "./axios";
-
-export const getStats = async (role) => {
-  const url = role === "Admin" ? "/stats/admin" : "/stats/employee";
-  const { data } = await api.get(url);
-  return data.data;
-};
